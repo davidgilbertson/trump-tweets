552fa3af14b65a3aaf1bf589bca49d541fc41344
David Gilbertson <gilbertson.david@gmail.com>
2017-09-04 18:33:56 +1000

    Add better 'wrong' answers (not using yet)

--- src/data/tweets.js
+++ src/data/tweets.js
@@ -8,6 +8,7 @@ export const TWEETS = [
     {
       text: 'Amazing how the haters & losers keep tweeting the name “F**kface Von Clownstick” like they are so original & like no one else is doing it...',
       url: 'https://twitter.com/realDonaldTrump/status/330359969813770242',
+      wrongMessage: 'Wrong, Clownstick!',
     },
     {
       text: 'Everyone keeps calling me stupid but they don\'t know that I am the greatest and I will make america great again. Go team!',
@@ -20,6 +21,7 @@ export const TWEETS = [
     {
       text: 'Healthy young child goes to doctor, gets pumped with massive shot of many vaccines, doesn\'t feel good and changes - AUTISM. Many such cases!',
       url: 'https://twitter.com/realDonaldTrump/status/449525268529815552',
+      wrongMessage: 'No, you vaxxer!',
     },
     {
       text: 'Healthy young child move to house under wind turbines. 6 months later, cancer. Happens all the time! JOBS FOR COAL!',
@@ -32,6 +34,7 @@ export const TWEETS = [
     {
       text: 'Sorry losers and haters, but my I.Q. is one of the highest - and you all know it! Please don\'t feel so stupid or insecure,it\'s not your fault',
       url: 'https://twitter.com/realDonaldTrump/status/332308211321425920',
+      wrongMessage: 'Wrong, loser',
     },
     {
       text: 'I have the highest IQ. My IQ is so high - it is one of the highest and you losers and haters are stupid and insucre because of low IQ!',
@@ -44,6 +47,7 @@ export const TWEETS = [
     {
       text: 'How can a dummy dope like Harry Hurt, who wrote a failed book about me but doesn\'t know me or anything about me, be on TV discussing Trump?',
       url: 'https://twitter.com/realdonaldtrump/status/626508731073867776',
+      wrongMessage: 'Dummy dope!',
     },
     {
       text: 'How can a stupid head like Barry Bunt, who wrote a failed book about horses but doesn\'t know anything about them, be on TV discussing them?',
@@ -56,6 +60,7 @@ export const TWEETS = [
     {
       text: 'Truly weird Senator Rand Paul of Kentucky reminds me of a spoiled brat without a properly functioning brain. He was terrible at DEBATE!',
       url: 'https://twitter.com/realdonaldtrump/status/630901938608017413',
+      wrongMessage: 'Wrong, weirdo!',
     },
     {
       text: 'Rand Paul is like a real loser. I don\'t think his brain works properly, he not very smart. MAKE AMERICA GREAT AGAIN!!!',
@@ -68,6 +73,7 @@ export const TWEETS = [
     {
       text: 'Huma Abedin, the top aide to Hillary Clinton and the wife of perv sleazebag Anthony Wiener, was a major security risk as a collector of info',
       url: 'https://twitter.com/realdonaldtrump/status/638318502059880450',
+      wrongMessage: 'Bup bow, sleazo',
     },
     {
       text: 'Huma Abedin, a muslim aide of Hillary Clinton, is as corrupt as Hillary and as stupid as husband Anthony Wiener. Easy competition!',
@@ -80,6 +86,7 @@ export const TWEETS = [
     {
       text: 'Arianna Huffington is unattractive both inside and out. I fully understand why her former husband left her for a man - he made a good decision.',
       url: 'https://twitter.com/realDonaldTrump/status/240462265680289792',
+      wrongMessage: 'You wrong and UGLY!',
     },
     {
       text: 'Arianna Huffington turned former husband gay in 11 years and sold out writers at FAKE HuffPo in 6 years. Will ruin California! Not trusted!',
@@ -92,6 +99,7 @@ export const TWEETS = [
     {
       text: 'Windmills are the greatest threat in the US to both bald and golden eagles. Media claims fictional \'global warming\' is worse',
       url: 'https://twitter.com/realDonaldTrump/status/509436043368873984',
+      wrongMessage: 'Poor Eagles',
     },
     {
       text: 'Solar panels are the greatest threat in the US to both squirrels and trash pandas. Fake news claims \'global warming\' is worse!',
@@ -104,6 +112,7 @@ export const TWEETS = [
     {
       text: 'Let\'s take a closer look at that birth certificate. Barack Obama was described in 2003 as being "born in Kenya."',
       url: 'https://twitter.com/realDonaldTrump/status/203568571148800001',
+      wrongMessage: 'Wrong again!',
     },
     {
       text: 'Let\'s learn a bit about history. Barack Obama says "born in Hawaii" - not a real American!',
@@ -116,6 +125,7 @@ export const TWEETS = [
     {
       text: 'If the morons who killed all of those people at Charlie Hebdo would have just waited, the magazine would have folded - no money, no success!',
       url: 'https://twitter.com/realDonaldTrump/status/555367040907612160',
+      wrongMessage: 'So wrong, moron.',
     },
     {
       text: 'Many businesses in the World Trade Center suffered that day. American business was interuppted, and lives!',
@@ -128,6 +138,7 @@ export const TWEETS = [
     {
       text: 'Every time I speak of the haters and losers I do so with great love and affection. They cannot help the fact that they were born fucked up!',
       url: 'https://twitter.com/realDonaldTrump/status/516382177798680576',
+      wrongMessage: 'Wrong. I love you.',
     },
     {
       text: 'Haters and losers are messed up but I say so with great fondness. They are my friends and we will MAKE AMERICA GREAT AGAIN!',
@@ -140,6 +151,7 @@ export const TWEETS = [
     {
       text: 'MAKE AMERICA GREAT AGAIN!',
       url: 'https://twitter.com/realdonaldtrump/status/884033889613828096',
+      wrongMessage: 'WRONG AGAIN!',
     },
     {
       text: 'make america great again!',
@@ -151,7 +163,8 @@ export const TWEETS = [
   [
     {
       text: 'Captured or not, all our soldiers are heroes!',
-      url: 'https://twitter.com/realdonaldtrump/status/622465729737138176'
+      url: 'https://twitter.com/realdonaldtrump/status/622465729737138176',
+      wrongMessage: 'Negative, soldier',
     },
     {
       text: 'You don\'t need to die to be a service to your country!',
@@ -164,6 +177,7 @@ export const TWEETS = [
     {
       text: 'This election is a total sham and a travesty. We are not a democracy!',
       url: 'https://twitter.com/realDonaldTrump/status/266035509162303492',
+      wrongMessage: 'Sham answer',
     },
     {
       text: 'Turkey\'s election was a total sham and a travesty. They are not a democracy!',
@@ -176,6 +190,7 @@ export const TWEETS = [
     {
       text: 'Obama is, without question, the WORST EVER president. I predict he will now do something really bad and totally stupid to show manhood!',
       url: 'https://twitter.com/realDonaldTrump/status/474719268819308544',
+      wrongMessage: 'WORST EVER answer',
     },
     {
       text: 'Obama does nothing but play golf and is the DUMBEST. I will repeal and replace Obamacare!',
@@ -188,6 +203,7 @@ export const TWEETS = [
     {
       text: 'If Obama resigns from office NOW, thereby doing a great service to the country — I will give him free lifetime golf at any one of my courses!',
       url: 'https://twitter.com/realDonaldTrump/status/509814075787051008',
+      wrongMessage: 'You should resign!',
     },
     {
       text: 'Obama should be impeached and will feel bad. Really. I will play golf with him and I think we can br friends. Not fit to run country though!',
@@ -200,6 +216,7 @@ export const TWEETS = [
     {
       text: 'I have never seen a thin person drinking Diet Coke.',
       url: 'https://twitter.com/realDonaldTrump/status/257552283850653696',
+      wrongMessage: 'Wrong, fatty',
     },
     {
       text: 'It\'s amazing that the amount of news that happens every day always just exactly fits the newspaper.',
@@ -212,6 +229,7 @@ export const TWEETS = [
     {
       text: 'Katy Perry, what the hell were you thinking when you married loser Russell Brand. There is a guy who has got nothing going, a waste!',
       url: 'https://twitter.com/realDonaldTrump/status/522897870891261952',
+      wrongMessage: 'Waste of an answer!',
     },
     {
       text: 'Why does Fred Durst argue with Carson Daly over who Christina Aguilera dated first',
@@ -224,6 +242,7 @@ export const TWEETS = [
     {
       text: 'The U.S. cannot allow EBOLA infected people back. People that go to far away places to help out are great-but must suffer the consequences!',
       url: 'https://twitter.com/realDonaldTrump/status/495379061972410369',
+      wrongMessage: 'Wrong. Suffer!',
     },
     {
       text: 'Ebola infected American made their choice going to Africa. It is very dirty and there is disease and they knew that. Can\'t bring that here!',
@@ -236,6 +255,7 @@ export const TWEETS = [
     {
       text: 'Pervert alert. Anthony Weiner is back on twitter. All girls under the age of 18, block him immediately.',
       url: 'https://twitter.com/realDonaldTrump/status/266276897305026560',
+      wrongMessage: 'No go, pervo.',
     },
     {
       text: 'Sound the Weiner siren, Kiddy fiddler Anthony Weiner is back on twitter! Mothers, hide your daughters.',
